3f2241e98355801f1ebaf6c735e94d85e2a1f54d
franco-fernettepearl <fernettepearl.franco55@gmail.com>
2025-07-02 23:07:00 +0800

    add route to list by id with todo

--- app.py
+++ app.py
@@ -11,5 +11,10 @@ def list_programs():
     #TODO(Franco): Implement logic to list all programs
     pass
 
+@app.route('/programs/<int:id>', methods=['GET'])
+def get_program(id):
+    #TODO(Franco): Implement logic to get a specific program by ID
+    pass
+
 if __name__ == "__main__":
     app.run(debug=True)
