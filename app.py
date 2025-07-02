193256ae89af6e048099bc70ef23e89dda9ae0cb
Fernette Pearl M. Franco <fernettepearl.franco55@gmail.com>
2025-07-02 23:40:16 +0800

    Flask structure (#2)
    * create flask structure
    
    * add home app route
    
    * add route to list all programs with todo
    
    * add route to list by id with todo
    
    * add route to list by year with todo
    
    * add route to list by name
    
    * add route to list by degree type with todo
    
    * add route that update a program with todo
    
    * add route to create a program with todo
    
    * add route to delete a program with todo
    
    * compact get methods in one function
    
    * isolate put and patch methods

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    pass
+
+def list_programs():
+    """
+    Handles:
+    - GET /programs → list all
+    - GET /programs?year_length=4
+    - GET /programs?name=BSIT
+    - GET /programs?degree_type=Bachelor
+    """
+    year_length = request.args.get('year_length')
+    name = request.args.get('name')
+    degree_type = request.args.get('degree_type')
+
+    # TODO(Franco): Implement logic to filter based on the above query params
+    pass
+
+@app.route('/programs/<int:id>', methods=['PUT'])
+def replace_program(id):
+    # TODO(Causon): Implement logic to fully replace a program
+    pass
+
+@app.route('/programs/<int:id>', methods=['PATCH'])
+def modify_program(id):
+    # TODO(Causon): Implement logic to partially update a program
+    pass
+
+@app.route('/programs', methods=['POST'])
+def create_program():
+    #TODO(Efondo): Implement logic to create a program
+    pass
+
+@app.route('/programs/<int:id>', methods=['DELETE'])
+def delete_program(id):
+    #TODO(Lopez): Implement logic to delete a program
+    pass
+
+if __name__ == "__main__":
+    app.run(debug=True)
