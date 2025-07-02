33d982b0622043a408a6e44022697e5fc26cc754
franco-fernettepearl <fernettepearl.franco55@gmail.com>
2025-07-02 23:06:01 +0800

    add route to list all programs with todo

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     pass
 
+@app.route('/programs', methods=['GET'])
+def list_programs():
+    #TODO(Franco): Implement logic to list all programs
+    pass
+
 if __name__ == "__main__":
     app.run(debug=True)
